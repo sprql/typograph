9a91cb3466aff94f57ea6ac946f97d4b16b73e7f
Alexander Obukhov <github@sprql.space>
2017-01-10 23:22:59 +0300

    Initialize project

---
+++ lib/typograph.ex
@@ -0,0 +1,18 @@
+defmodule Typograph do
+  @moduledoc """
+  Documentation for Typograph.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Typograph.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/typograph_test.exs
@@ -0,0 +1,8 @@
+defmodule TypographTest do
+  use ExUnit.Case
+  doctest Typograph
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
